6cb22da0510c237ceb708c434c25e6f93005691c
Esteban-Dds <ztheeban205@gmail.com>
2020-10-15 16:48:34 -0500

    interfaz admin

--- principal/admin.py
+++ principal/admin.py
@@ -2,6 +2,13 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
+# Configuración del panel
+titulo = "Panel de Gestión"
+subTitulo = "Proyecto Pravaz"
+
+admin.site.site_header = titulo
+admin.site.site_title  = titulo 
+admin.site.index_title = subTitulo
 
 admin.site.register(Roles)
 admin.site.register(Medicos)
--- principal/views.py
+++ principal/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout #  son necesarios para el inicio de sesion
+from django.contrib.auth.forms import AuthenticationForm
+
 
 from .forms import RegistroPacientes
 from .models import Pacientes
@@ -52,8 +55,24 @@ def pagina_registrarse(request):
     return render(request,'usuario/registro.html',context)  
 
 def inicio_sesion(request):
+
+    if request.method == 'POST':
+        usuario = request.POST.get('usuario') # recogemos el dato que nos llega en el name de los campos del formulario
+        clave_paciente = request.POST.get('clave') 
+
+        # paciente = authenticate(request, nombre_paciente=usuario, clave_paciente=contrasena)
+
+        # print(clave)
+        # if contrasena == clave:
+        #     print("La clave")
+
+        # if paciente is not None:
+        #     login(request, paciente) #esto nos crea la sesion de usuario. Le pasamos el request y el usuario que queremos identificar
+        #     return redirect('usuario')
+        # else:
+        #     messages.warning(request, 'Credenciales incorrectas')
+
     context = {
-        'titulo': 'Iniciar Sesión',
-        # 'formulario_registro': formulario_registro
-    }  
+        'title': 'Iniciar Sesión'
+    }
     return render(request, 'usuario/inicio_sesion.html', context)
