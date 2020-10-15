6fcd604fe74c82a1d6e766f4fb3da74216c67abc
Esteban-Dds <ztheeban205@gmail.com>
2020-10-15 09:46:03 -0500

    Vistas

--- principal/views.py
+++ principal/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def inicio(request):
+
+
+    context = {
+        'titulo': 'Inicio'
+    }
+    return render(request, 'inicio.html', context)
+
+def medico(request)
+
+    context = {
+        'titulo': 'Médico'
+    }
+    return render(request, 'inicio.html', context)
+
+def usuario(request)
+
+    context = {
+        'titulo': 'Usuario'
+    }
+    return render(request, 'inicio.html', context)
